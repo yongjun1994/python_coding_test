0e0000155b1bc75c5875df43253410d129a17fd9
Yong Jun Lee (Michael) <iyongjun089@gmail.com>
2025-08-12 09:27:19 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.00 ms, Memory: 9.25 MB -BaekjoonHub

---
+++ 프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.py
@@ -0,0 +1,9 @@
+def solution(price):
+    if price >= 500000:      
+        return price * 80 // 100
+    elif price >= 300000:     
+        return price * 90 // 100
+    elif price >= 100000:     
+        return price * 95 // 100
+    else:                     
+        return price
